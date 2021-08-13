968313779bd239f93a208b53acd637027c07149a
Takuya Matsuyama <hi@craftz.dog>
2021-08-13 16:53:23 +0900

    Reseting project to the author's original due inconsistencies

--- packages/renderer/src/use-codemirror.tsx
+++ packages/renderer/src/use-codemirror.tsx
@@ -1,58 +1,59 @@
-import { useEffect, useState, useRef } from "react";
-import { EditorState } from '@codemirror/state';
+import { useEffect, useState, useRef } from 'react'
+import { EditorState } from '@codemirror/state'
 import { EditorView, keymap, highlightActiveLine } from '@codemirror/view'
 import { defaultKeymap } from '@codemirror/commands'
-import { history, historyKeymap } from "@codemirror/commands";
+import { history, historyKeymap } from '@codemirror/history'
 import { indentOnInput } from '@codemirror/language'
 import { bracketMatching } from '@codemirror/matchbrackets'
 import { lineNumbers, highlightActiveLineGutter } from '@codemirror/gutter'
-import { defaultHighlightStyle } from "@codemirror/highlight";
+import { defaultHighlightStyle, HighlightStyle, tags } from '@codemirror/highlight'
 import { javascript } from '@codemirror/lang-javascript'
 import type React from 'react'
 
 interface Props {
-    initialDoc: string,
-    onChange?: (state: EditorState) => void
+  initialDoc: string,
+  onChange?: (state: EditorState) => void
 }
 
 const useCodeMirror = <T extends Element>(
-    props: Props
+  props: Props
 ): [React.MutableRefObject<T | null>, EditorView?] => {
-    const refContainer = useRef<T>(null)
-    const [editorView, setEditorView] = useState<EditorView>()
-    useEffect(() => {
-        if (!refContainer.current) return 
+  const refContainer = useRef<T>(null)
+  const [editorView, setEditorView] = useState<EditorView>()
+  const { onChange } = props
 
-        const startState = EditorState.create({
-            doc: props.initialDoc,
-            extensions: [
-                keymap.of([...defaultKeymap, ...historyKeymap]),
-                lineNumbers(),
-                highlightActiveLineGutter(),
-                history(),
-                indentOnInput(),
-                bracketMatching(),
-                defaultHighlightStyle.fallback,
-                highlightActiveLine(),
-                javascript(),
-                EditorView.lineWrapping,
-                EditorView.updateListener.of(update => {
-                    if (update.changes) {
-                        onChange && onChange(update.state)
-                    }
-                })
-            ]  
+  useEffect(() => {
+    if (!refContainer.current) return
+
+    const startState = EditorState.create({
+      doc: props.initialDoc,
+      extensions: [
+        keymap.of([...defaultKeymap, ...historyKeymap]),
+        lineNumbers(),
+        highlightActiveLineGutter(),
+        history(),
+        indentOnInput(),
+        bracketMatching(),
+        defaultHighlightStyle.fallback,
+        highlightActiveLine(),
+        javascript(),
+        EditorView.lineWrapping,
+        EditorView.updateListener.of(update => {
+          if (update.changes) {
+            onChange && onChange(update.state)
+          }
         })
+      ]
+    })
 
-        const view = new EditorView({
-            state: startState,
-            parent: refContainer.current
-        })
-        setEditorView(view)
+    const view = new EditorView({
+      state: startState,
+      parent: refContainer.current
+    })
+    setEditorView(view)
+  }, [refContainer])
 
-    }, [refContainer])
-
-    return [refContainer, editorView]
+  return [refContainer, editorView]
 }
 
 export default useCodeMirror
